0a4bdb577e1020468902ab4c9400c5002658c467
Carlton Perkins <carltonrp52@gmail.com>
2021-01-26 12:16:50 -0500

    3 - Build a test suite of a dozen R0 programs.

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,37 @@
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 enum Expr<'a> {
     Num(isize),
     Read,
     Negate(&'a Expr<'a>),
-    Add(&'a Expr<'a>, &'a Expr<'a>)
+    Add(&'a Expr<'a>, &'a Expr<'a>),
 }
-#[derive(Debug)]
-struct Program<'a> {
-    e: Expr<'a>,
-}
+use Expr::*;
 
+use Expr as Program;
+
+fn interp(prog: Program) -> isize {
+    0
+}
 
 fn main() {
-    println!("{:?}", Expr::Num(5));
-    println!("{:?}", Expr::Add(&Expr::Num(5), &Expr::Num(6)));
+    println!("{:?}", Num(5));
+    println!("{:?}", Add(&Num(5), &Num(6)));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_basic() {
+        assert_eq!(interp(Num(5)), 5);
+        assert_eq!(interp(Num(-5)), -5);
+        assert_eq!(interp(Add(&Num(5), &Num(6))), 11);
+        assert_eq!(interp(Read), 0);
+        assert_eq!(interp(Negate(&Num(5))), -5);
+        assert_eq!(interp(Add(&Num(5), &Negate(&Num(6)))), -1);
+        assert_eq!(interp(Add(&Read, &Negate(&Num(6)))), -6);
+        assert_eq!(interp(Negate(&Negate(&Negate(&Num(6))))), -6);
+        assert_eq!(interp(Negate(&Negate(&Negate(&Num(0))))), 0);
+    }
 }
