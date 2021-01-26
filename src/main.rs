2636421878d1d440580502dcb1e08a9248e9687d
Carlton Perkins <carltonrp52@gmail.com>
2021-01-26 11:38:13 -0500

    2 - Write a pretty-printer for R0 programs.

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,17 @@
+#[derive(Debug)]
 enum Expr<'a> {
     Num(isize),
     Read,
     Negate(&'a Expr<'a>),
     Add(&'a Expr<'a>, &'a Expr<'a>)
 }
-
+#[derive(Debug)]
 struct Program<'a> {
     e: Expr<'a>,
 }
 
 
 fn main() {
-    println!("Hello, world!");
+    println!("{:?}", Expr::Num(5));
+    println!("{:?}", Expr::Add(&Expr::Num(5), &Expr::Num(6)));
 }
