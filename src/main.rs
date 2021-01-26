1fb2c2985b7573f4179a8cb8091ec18395a71057
Carlton Perkins <carltonrp52@gmail.com>
2021-01-26 11:34:21 -0500

    1 - Define data types for R0 program ASTs.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+enum Expr<'a> {
+    Num(isize),
+    Read,
+    Negate(&'a Expr<'a>),
+    Add(&'a Expr<'a>, &'a Expr<'a>)
+}
+
+struct Program<'a> {
+    e: Expr<'a>,
+}
+
+
 fn main() {
     println!("Hello, world!");
 }
