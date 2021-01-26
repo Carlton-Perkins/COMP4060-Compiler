01cee980e0dd3f78e8f71bad31db39b91c73e0d5
Carlton Perkins <carltonrp52@gmail.com>
2021-01-26 12:54:08 -0500

    4 - Write an interpreter for R0 programs

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Copy, Clone)]
 enum Expr<'a> {
     Num(isize),
     Read,
@@ -6,32 +6,59 @@ enum Expr<'a> {
     Add(&'a Expr<'a>, &'a Expr<'a>),
 }
 use Expr::*;
+use isize as OType;
 
-use Expr as Program;
+struct Env {
+    read_count: isize,
+}
 
-fn interp(prog: Program) -> isize {
-    0
+impl Env {
+    fn new() -> Self {
+        Env{read_count: 0}    
+    }
+}
+
+type Program<'a> = (Expr<'a>, &'a mut Env);
+
+fn interp((expr, mut env): Program) -> OType {
+    match expr {
+        Num(n) => { n }
+        Read => { let res = env.read_count; env.read_count += 1; res }
+        Negate(ex) => { -1 * interp((*ex, &mut env))}
+        Add(lh, rh) => { interp((*lh, &mut env)) + interp((*rh, &mut  env))}
+    }
 }
 
 fn main() {
     println!("{:?}", Num(5));
     println!("{:?}", Add(&Num(5), &Num(6)));
+    println!("{:?}", Add(&Read, &Negate(&Num(6))));
+    println!("{:?}", interp((Add(&Read, &Negate(&Num(6))), &mut Env::new())));
+    
+
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
+    fn a_interp(expr: Expr, expect:OType) {
+        let prog = (expr, &mut Env::new());
+        let res = interp(prog);
+        assert_eq!(res, expect);
+    }
+
     #[test]
     fn test_basic() {
-        assert_eq!(interp(Num(5)), 5);
-        assert_eq!(interp(Num(-5)), -5);
-        assert_eq!(interp(Add(&Num(5), &Num(6))), 11);
-        assert_eq!(interp(Read), 0);
-        assert_eq!(interp(Negate(&Num(5))), -5);
-        assert_eq!(interp(Add(&Num(5), &Negate(&Num(6)))), -1);
-        assert_eq!(interp(Add(&Read, &Negate(&Num(6)))), -6);
-        assert_eq!(interp(Negate(&Negate(&Negate(&Num(6))))), -6);
-        assert_eq!(interp(Negate(&Negate(&Negate(&Num(0))))), 0);
+        a_interp(Num(5), 5);
+        a_interp(Num(-5), -5);
+        a_interp(Add(&Num(5), &Num(6)), 11);
+        a_interp(Add(&Read, &Read), 1);
+        a_interp(Read, 0);
+        a_interp(Negate(&Num(5)), -5);
+        a_interp(Add(&Num(5), &Negate(&Num(6))), -1);
+        a_interp(Add(&Read, &Negate(&Num(6))), -6);
+        a_interp(Negate(&Negate(&Negate(&Num(6)))), -6);
+        a_interp(Negate(&Negate(&Negate(&Num(0)))), 0);
     }
 }
