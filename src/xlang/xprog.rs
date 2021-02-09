de3fb717ff9ecb9cb662c9b0dcf50f9f7b26c7de
Carlton Perkins <carltonrp52@gmail.com>
2021-02-08 21:42:56 -0500

    21 - Write an interpreter for X0 programs.

--- src/xlang/xprog.rs
+++ src/xlang/xprog.rs
@@ -5,13 +5,37 @@ trait Emit {
     fn emit(&self) -> String;
 }
 
+trait Interp {
+    fn interp(&self, env: &Env) -> Env;
+}
+
 const NEWLINE: &str = "\n\r";
+
+#[derive(Clone)]
+struct Env {
+    register: HashMap<Register, OType>,
+    variable: HashMap<Var, OType>,
+    memory: HashMap<Address, OType>,
+    block: Program,
+}
+
+impl Env {
+    fn new(prog: Program) -> Self {
+        Env {
+            register: HashMap::new(),
+            variable: HashMap::new(),
+            memory: HashMap::new(),
+            block: prog,
+        }
+    }
+}
 
 type Var = usize;
 
 type OType = i64;
 
 type Label = String;
+type Address = usize;
 
 impl Emit for Label {
     fn emit(&self) -> String {
@@ -19,7 +43,19 @@ impl Emit for Label {
     }
 }
 
-#[derive(Debug, strum_macros::ToString)]
+impl Interp for Label {
+    fn interp(&self, env: &Env) -> Env {
+        let get = env.block.get(self).clone();
+        match get {
+            Some(blk) => blk.clone().interp(env),
+            None => {
+                panic!("No label {} found in program", self)
+            }
+        }
+    }
+}
+
+#[derive(Debug, strum_macros::ToString, PartialEq, Eq, Hash, Clone, Copy)]
 enum Register {
     RSP,
     RBP,
@@ -45,6 +81,7 @@ impl Emit for Register {
     }
 }
 
+#[derive(Clone, Copy, Debug)]
 enum Argument {
     Con(OType),
     Reg(Register),
@@ -69,6 +106,7 @@ impl Emit for Argument {
     }
 }
 
+#[derive(Clone, Debug)]
 enum Instruction {
     Addq(Argument, Argument),
     Subq(Argument, Argument),
@@ -104,6 +142,84 @@ impl Emit for Instruction {
     }
 }
 
+fn set(dst: &Argument, val: &OType, env: &Env) -> Env {
+    match dst {
+        Argument::Con(con) => {
+            panic!("Tried to set to a constant {} -> {:?}", con, dst)
+        }
+        Argument::Reg(reg) => {
+            let mut env_c = env.clone();
+            env_c.register.insert(*reg, *val);
+            env_c
+        }
+        Argument::Deref(reg, offset) => {
+            let mut env_c = env.clone();
+            let reg_val = env_c.register.get(reg).unwrap_or(&0); // Default registers to 0
+            let target = reg_val + offset;
+            env_c.memory.insert(target as usize, *val);
+            env_c
+        }
+        Argument::Ref(var) => {
+            let mut env_c = env.clone();
+            env_c.memory.insert(*var, *val);
+            env_c
+        }
+    }
+}
+
+fn value(arg: &Argument, env: &Env) -> OType {
+    match arg {
+        Argument::Con(c) => *c,
+        Argument::Reg(reg) => *env.register.get(reg).unwrap_or(&0), // Default registers to 0
+        Argument::Deref(reg, offset) => {
+            let reg_val = env.register.get(reg).unwrap_or(&0); // Default registers to 0
+            let target = reg_val + offset;
+            *env.memory.get(&(target as usize)).unwrap_or(&0) // Defaults memory to 0
+        }
+        Argument::Ref(var) => {
+            *env.variable.get(var).unwrap_or(&0) // Default variables to 0
+        }
+    }
+}
+
+fn push(src: &Argument, env: &Env) -> Env {
+    let mut env_c = env.clone();
+    let val = value(src, &env_c);
+    env_c = set(
+        &Argument::Reg(Register::RSP),
+        &(value(&Argument::Deref(Register::RSP, 0), &env_c) - 8),
+        &env_c,
+    );
+    set(&Argument::Deref(Register::RSP, 0), &val, &env_c)
+}
+
+fn pop(dst: &Argument, env: &Env) -> Env {
+    let mut env_c = env.clone();
+    let val = value(&Argument::Deref(Register::RSP, 0), &env_c);
+    env_c = set(dst, &val, &env_c);
+    set(
+        &Argument::Reg(Register::RSP),
+        &(value(&Argument::Reg(Register::RSP), &env_c) + 8),
+        &env_c,
+    )
+}
+
+impl Interp for Instruction {
+    fn interp(&self, env: &Env) -> Env {
+        match self {
+            Instruction::Addq(src, dst) => set(dst, &(value(src, env) + value(dst, env)), env),
+            Instruction::Subq(src, dst) => set(dst, &(value(src, env) - value(dst, env)), env),
+            Instruction::Movq(src, dst) => set(dst, &value(src, env), env),
+            Instruction::Retq => env.clone(),
+            Instruction::Negq(v) => set(v, &value(v, env), env),
+            Instruction::Callq(l) => l.interp(env),
+            Instruction::Jmp(l) => l.interp(env),
+            Instruction::Pushq(src) => push(src, env),
+            Instruction::Popq(dst) => pop(dst, env),
+        }
+    }
+}
+
 type Block = Vec<Instruction>;
 
 impl Emit for Block {
@@ -114,6 +230,19 @@ impl Emit for Block {
     }
 }
 
+impl Interp for Block {
+    fn interp(&self, env: &Env) -> Env {
+        match self.split_first() {
+            Some((first, rest)) => rest
+                .into_iter()
+                .map(|x| x.clone())
+                .collect::<Block>()
+                .interp(&first.interp(&env)),
+            None => env.clone(),
+        }
+    }
+}
+
 type Program = HashMap<Label, Block>;
 
 impl Emit for Program {
@@ -121,6 +250,13 @@ impl Emit for Program {
         self.into_iter()
             .map(|(lab, blk)| format!("{0}:{1}{2}{1}{1}", lab.emit(), NEWLINE, blk.emit()))
             .fold("".to_string(), |acc, s| acc + &s)
+    }
+}
+
+impl Interp for Program {
+    fn interp(&self, _: &Env) -> Env {
+        let env = Env::new(self.clone());
+        Label::from("_main").interp(&env)
     }
 }
 
@@ -145,8 +281,7 @@ mod test_xprog {
         assert_eq!(Argument::Deref(Register::RAX, 5).emit(), "%RAX(5)");
     }
 
-    #[test]
-    fn test_xprog() {
+    fn get_test_progs() -> Vec<(Program, i64)> {
         let test_progs: Vec<(Program, i64)> = vec![
             (
                 vec![(
@@ -180,10 +315,34 @@ mod test_xprog {
                 .collect::<HashMap<_, _>>(),
                 33,
             ),
+            (
+                vec![(
+                    "_main".to_string(),
+                    vec![
+                        Movq(Con(5), Reg(RAX)),
+                        Movq(Con(6), Reg(R9)),
+                        Pushq(Reg(R9)),
+                        Popq(Reg(RAX)),
+                        Retq,
+                    ],
+                )]
+                .into_iter()
+                .collect::<HashMap<_, _>>(),
+                6,
+            ),
         ];
 
-        for (test_prog, expected_res) in test_progs {
+        test_progs
+    }
+
+    #[test]
+    fn test_xprog() {
+        for (test_prog, expected_res) in get_test_progs() {
             let res = compile_and_run(&test_prog);
+            let interp_env = Env::new(HashMap::new());
+            let res_env = test_prog.interp(&interp_env);
+
+            assert_eq!(res_env.register[&RAX], expected_res);
 
             match res {
                 Ok(n) => {
@@ -209,6 +368,23 @@ mod test_xprog {
                     );
                 }
             }
+        }
+    }
+
+    #[test]
+    fn test_xprog_interp() {
+        for (test_prog, expected_res) in get_test_progs() {
+            let interp_env = Env::new(HashMap::new());
+            let res_env = test_prog.interp(&interp_env);
+
+            assert_eq!(
+                value(&Reg(RAX), &res_env),
+                expected_res,
+                "Program returned {}, when it should have returned {}: {:?}",
+                value(&Reg(RAX), &res_env),
+                expected_res,
+                test_prog
+            );
         }
     }
 }
