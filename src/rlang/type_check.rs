38bc441cecc9c4bc005e7f934b656c0e56a08fa2
Carlton Perkins <carltonrp52@gmail.com>
2021-04-06 14:30:50 -0400

    72 - Write a type-checker for R2.

--- src/rlang/type_check.rs
+++ src/rlang/type_check.rs
@@ -1,8 +1,10 @@
-use super::RProgram;
-use crate::common::types::Variable;
+use crate::{
+    common::types::Variable,
+    rlang::{RExpr::*, RProgram},
+};
 use std::collections::HashMap;
 
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
 enum Type {
     S64,
     Bool,
@@ -17,11 +19,71 @@ struct TypeCheckEnv {
 
 trait TypeCheck {
     fn typec(&self) -> Result<Type, TypeCheckError>;
+    fn typec_(&self, env: &mut TypeCheckEnv) -> Result<Type, TypeCheckError>;
 }
 
 impl TypeCheck for RProgram {
     fn typec(&self) -> Result<Type, TypeCheckError> {
-        todo!()
+        self.typec_(&mut TypeCheckEnv::new())
+    }
+
+    fn typec_(&self, env: &mut TypeCheckEnv) -> Result<Type, TypeCheckError> {
+        use Type::*;
+        match self {
+            RNum(_) => Ok(S64),
+            RRead => Ok(S64),
+            RNegate(e) => match e.typec_(env)? {
+                S64 => Ok(S64),
+                Bool => Err(TypeCheckError {}),
+            },
+            RAdd(l, r) => {
+                let lt = l.typec_(env)?;
+                let rt = r.typec_(env)?;
+                match lt == rt {
+                    true => Ok(lt),
+                    false => Err(TypeCheckError {}),
+                }
+            }
+            RLet(c, ve, be) => {
+                let vt = ve.typec_(env)?;
+                env.var_type.insert(c.clone(), vt);
+                be.typec_(env)
+            }
+            RVar(v) => Ok(env
+                .var_type
+                .get(v)
+                .expect("Typec: Unbound variable")
+                .clone()),
+            RBool(_) => Ok(Bool),
+            RCmp(_, l, r) => {
+                let lt = l.typec_(env)?;
+                let rt = r.typec_(env)?;
+                match lt == rt {
+                    true => Ok(Bool),
+                    false => Err(TypeCheckError {}),
+                }
+            }
+            RIf(c, t, f) => {
+                if c.typec_(env)? != Bool {
+                    Err(TypeCheckError {})
+                } else {
+                    let tt = t.typec_(env)?;
+                    let ft = f.typec_(env)?;
+                    match tt == ft {
+                        true => Ok(tt),
+                        false => Err(TypeCheckError {}),
+                    }
+                }
+            }
+        }
+    }
+}
+
+impl TypeCheckEnv {
+    fn new() -> Self {
+        TypeCheckEnv {
+            var_type: HashMap::new(),
+        }
     }
 }
 
@@ -29,7 +91,6 @@ impl TypeCheck for RProgram {
 mod test_typecheck {
     use super::Type::*;
     use super::*;
-    use crate::rlang::RExpr::*;
 
     #[test]
     fn test_typec_r2() {
@@ -52,6 +113,7 @@ mod test_typecheck {
 
         for (prog, expected_type) in progs {
             let tr = prog.typec();
+            println!("{:?}", prog);
             assert_eq!(tr, expected_type);
         }
     }
